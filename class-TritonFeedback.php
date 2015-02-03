4464ef3ebdfe8396a3452eea1352ec60350e2788
Tom <thomas.borger@gmail.com>
2015-02-03 11:30:00 -0800

    Workflow and folder structure

--- class-TritonFeedback.php
+++ class-TritonFeedback.php
@@ -33,7 +33,7 @@ class TritonFeedback{
 		wp_register_script( 'angularjs', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.min.js', array(), null, true );
 		wp_register_script( 'firebase', 'https://cdn.firebase.com/js/client/2.0.4/firebase.js', array( 'angularjs' ), null, true );
 		wp_register_script( 'angularfire', 'https://cdn.firebase.com/libs/angularfire/0.9.2/angularfire.min.js', array( 'angularjs', 'firebase' ), null, true );
-		wp_register_script( 'tritonfeedback', plugins_url( 'dist/tritonfeedback.min.js', __FILE__ ), array( 'angularjs', 'firebase', 'angularfire' ), null, true );
+		wp_register_script( 'tritonfeedback', plugins_url( 'js/tritonfeedback.min.js', __FILE__ ), array( 'angularjs', 'firebase', 'angularfire' ), null, true );
 	}
 
 	public function enqueue_frontend(){
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require( 'gulp' );
+var browserSync = require( 'browser-sync' );
+var plumb = require( 'gulp-plumber' );
+var jshint = require( 'gulp-jshint' );
+var annotate = require( 'gulp-ng-annotate' );
+var add = require( 'gulp-add-src' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+var chmod = require( 'gulp-chmod' ); 
+var sass = require( 'gulp-sass' );
+var filter = require( 'gulp-filter' );
+
+
+/* -------------------------------------------------------------------------- *
+ * CONFIG
+ * -------------------------------------------------------------------------- */
+var config = {
+	sass: {
+		watch: 'sass/**/*.scss',
+		src: 'sass/main.scss',
+		dest: 'css'
+	},
+	js: {
+		watch: 'src/**/*.js',
+		deps: [
+			'lib/jquery-ui.min.js',
+			'lib/jquery.ui.touch-punch.js',
+			'lib/jquery.videocover.js',
+			'lib/jquery.sticky-kit.min.js',
+			'lib/jquery.waypoints.min.js',
+			'lib/getEmPixels.min.js',
+		],
+		src: [
+			'src/app.js',
+			'src/*/*.js'
+		],
+		dest: 'js',
+		name: 'tritonfeedback.min.js'
+	},
+};  
+
+
+/* -------------------------------------------------------------------------- *
+ * BROWSERSYNC SERVER
+ * -------------------------------------------------------------------------- */
+gulp.task( 'sync', function() {
+	browserSync({
+		proxy: 'tritonday.loc/'
+	});
+});
+
+
+/* -------------------------------------------------------------------------- *
+ * JS
+ *
+ * We are trying to keep Browserify out of this. It's just a pain in the butt
+ * with Angular. So instead we just concatenate in order: dependencies,
+ * non-Angular modules, the Angular app definition, and then Angular modules.
+ * -------------------------------------------------------------------------- */
+gulp.task( 'js', function(){
+
+	gulp.src( config.js.src )
+		.pipe( plumb() )
+		.pipe( jshint() )
+		.pipe( jshint.reporter('default') )
+		.pipe( annotate() )
+		.pipe( add.prepend( config.js.deps ) )
+		.pipe( concat( config.js.name ) )
+		.pipe( uglify() )
+		.pipe( chmod( 755 ) )
+		.pipe( gulp.dest( config.js.dest ) );
+		browserSync.reload();
+}); 
+
+
+/* -------------------------------------------------------------------------- *
+ * SASS
+ * -------------------------------------------------------------------------- */
+gulp.task( 'sass', function(){
+  return gulp.src( config.sass.src )
+    .pipe( sass({ outputStyle: 'compressed', errLogToConsole: true }) )
+    .pipe( gulp.dest( config.sass.dest ) )
+		.pipe( filter( '**/*.css' ) )
+    .pipe( browserSync.reload({ stream:true }) );
+});
+
+
+/* -------------------------------------------------------------------------- *
+ * WATCH
+ * -------------------------------------------------------------------------- */
+gulp.task( 'watch', function(){
+	gulp.watch( config.sass.watch, ['sass'] );
+	gulp.watch( config.js.watch, ['js'] );
+});
+
+
+/* -------------------------------------------------------------------------- *
+ * GO GO GADGET GULP
+ * -------------------------------------------------------------------------- */
+gulp.task( 'default', ['sync', 'watch'] );
+
