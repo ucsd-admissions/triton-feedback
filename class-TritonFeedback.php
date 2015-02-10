60dc639d228babdcd12e31304e815edd0f028c3a
Tom <thomas.borger@gmail.com>
2015-02-09 16:40:13 -0800

    Downgrade to 1.2.28 to support IE8

--- class-TritonFeedback.php
+++ class-TritonFeedback.php
@@ -18,8 +18,8 @@ class TritonFeedback{
 	}
 
 	public function register_frontend(){
-		wp_register_script( 'angularjs', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.min.js', array(), null, true );
-		wp_register_script( 'angular-cookies', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular-cookies.js', array( 'angularjs' ), null, true );
+		wp_register_script( 'angularjs', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular.min.js', array(), null, true );
+		wp_register_script( 'angular-cookies', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular-cookies.js', array( 'angularjs' ), null, true );
 		wp_register_script( 'firebase', 'https://cdn.firebase.com/js/client/2.0.4/firebase.js', array( 'angularjs' ), null, true );
 		wp_register_script( 'angularfire', 'https://cdn.firebase.com/libs/angularfire/0.9.2/angularfire.min.js', array( 'angularjs', 'firebase' ), null, true );
 		wp_register_script( 'triton-feedback', plugins_url( 'js/tritonfeedback.min.js', __FILE__ ), array( 'angularjs', 'angular-cookies', 'firebase', 'angularfire' ), null, true );
